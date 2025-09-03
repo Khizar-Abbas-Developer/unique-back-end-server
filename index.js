c1b621bebc8cb832667a267bd2c2baf184713081
Khizar-Abbas-Developer <khizarnot7@gmail.com>
2025-09-03 06:24:08 +0500

    ددددددددددددددd

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ const start = async () => {
     });
 
     // MongoDB Connection
+    // MongoDB Connection
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
